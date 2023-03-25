85dd6c8cc0c621379226a9bfe765dd8ade5d06a6
FearsRedemption <76001262+FearsRedemption@users.noreply.github.com>
2023-03-25 13:50:14 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+//assume another function will pull data from a database but using this as placeholder
+const json = "{\"Texas\":[59710,68240,76770,85300,92124,98948,105772,112596],\"RhodeIsland\":[69510,79440,89370,99300,107244,115188,123132,131076],\"Massachusetts\":[84280,96320,108360,120400,130032,139664,149296,158928]}";
+
+function validate(state, householdSize, annualIncome) {
+    let data = JSON.parse(json);
+    return annualIncome <= data[state][householdSize - 1];
+}
